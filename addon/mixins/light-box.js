9dd56e3cbc75146d2840f972b3deea69b1474ad8
Brandon Blaylock <brandon.blaylock@gmail.com>
2015-05-13 22:02:38 -0500

    modified so that the href with bind to the property

--- addon/mixins/light-box.js
+++ addon/mixins/light-box.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
     tagName: 'a',
-    attributeBindings: ['href', 'data-lightbox', 'data-title', 'data-class'],
+    attributeBindings: ['passedHref:href', 'data-lightbox', 'data-title', 'data-class'],
     inlineImage: true,
     classNames: ['ember-lightbox'],
     classNameBindings: ['inlineImage']
